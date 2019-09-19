ce7ebfb40974cfdb91bdf58b03ebd46583027b15
jagdish_o2onelabs <JDchauhan@github.com>
2019-09-19 13:05:10 +0530

    fix

---
+++ Config/App.js
@@ -0,0 +1,6 @@
+'use strict';
+
+module.exports = {
+    port: process.env.PORT || 3000,
+    JWT_SECRET: 'this is secret key for jwt'
+};
---
+++ Config/index.js
@@ -0,0 +1,6 @@
+'use strict';
+
+module.exports = {
+    Database: require('./Database'),
+    App: require('./App'),
+};
